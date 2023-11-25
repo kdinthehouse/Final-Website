560eff30bfc69a8eac7e1790411cfac7d151d882
realkdhub <krishnadas2k14@gmail.com>
2023-11-25 21:31:59 +0530

    s

---
+++ send-message.php
@@ -0,0 +1,27 @@
+<?php
+
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  // Replace these with your actual email address and recipient email address
+  $senderEmail = "seplu84713@appsamo.com";
+  $recipientEmail = "seplu84713@appsamo.com";
+
+  $subject = "Contact Form Submission";
+
+  $emailBody = "Name: $name\n\nEmail: $email\n\nMessage:\n$message";
+
+  $headers = "From: $senderEmail\r\n";
+
+  if (mail($recipientEmail, $subject, $emailBody, $headers)) {
+    echo "Message sent successfully!";
+  } else {
+    echo "Failed to send message. Please try again later.";
+  }
+} else {
+  echo "Invalid form submission.";
+}
+
+?>
